7996328a2d47d2a0149c68bb80193800090198fe
SchmidtRichard <ri.schmidta@gmail.com>
2020-07-31 12:01:04 +0100

    Step 4 - Check Which Button is Pressed

--- game.js
+++ game.js
@@ -1,8 +1,41 @@
+//Array to hold values of userChosenColour variable
+var userClickedPattern = [];
+
 //Array to hold the game pattern
 var gamePattern = [];
 
 //Array to hold the colors
 var buttonColours = ["red", "blue", "green", "yellow"];
+
+//jQuery to detect when any of the buttons are clicked and trigger a handler function
+$(".btn").click(function() {
+
+  //var userChosenColour holds the id of the button that got clicked
+
+  /*
+  A webpage is filled with objects (button, input, h1, p, ...).
+
+  Whenever the user performs an action (click, keypress,... ) on one of these objects, the objects on which the action took place is referred to as this (object) in the code.
+
+  You can compare it to the following:
+
+  - there is a piece of paper on the table.
+
+  - I take the piece of paper (action) and I call it this
+
+  - I can then go over it's properties (this is white, this is blank/written on, this weighs 80 gr/m2, ...)
+
+  So the object (the piece of paper) is referred to as this
+  */
+
+  var userChosenColour = $(this).attr("id");
+
+  //Add the contents of the variable userChosenColour to the end of userClickedPattern array
+  userClickedPattern.push(userChosenColour);
+
+  console.log(userClickedPattern);
+
+});
 
 //Function to generate a random number between 0 and 3
 function nextSequence() {
@@ -21,6 +54,6 @@ function nextSequence() {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 
-  return randomChosenColour;
+  //return randomChosenColour;
 
 }
